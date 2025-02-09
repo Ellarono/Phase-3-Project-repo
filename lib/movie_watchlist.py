9e416267a8d61435de3da75b70ee15b64776a272
Daniella Rono <daniellarono@gmail.com>
2025-02-10 01:15:00 +0300

    text adjustment

--- lib/movie_watchlist.py
+++ lib/movie_watchlist.py
@@ -15,7 +15,7 @@ def display_menu():
     print(Fore.GREEN + "4. Mark as Watched")
     print(Fore.MAGENTA + "5. Delete a Movie")
     print(Fore.CYAN + "6. Exit")
-    print(Fore.CYAN + "=" * 30)
+    print(Fore.CYAN + "_" * 30)
 
 def add_movie():
     """Add a new movie to the database."""
